c0028ed40e63af17db6101977818751a1eb00e00
Terry <terryakishin0814@gmail.com>
2022-12-11 23:17:07 +0900

    refactor(ApiRequestService) : getBlogArticleSortBySim 메서드 내부에서 lamba로 list 형변환 수정

--- export-api/src/main/java/com/myservice/service/ApiRequestService.java
+++ export-api/src/main/java/com/myservice/service/ApiRequestService.java
@@ -6,19 +6,14 @@ import com.myservice.domain.api.entity.ApiResponse;
 import com.myservice.domain.api.entity.ComposedKey;
 import com.myservice.domain.api.repository.ApiResponseRepository;
 import com.myservice.domain.api.util.ApiResponseParser;
-import com.myservice.domain.article.dto.BlogResultSaveDto;
-import com.myservice.domain.article.entity.ApiComposedKey;
 import com.myservice.domain.article.repository.BlogArticleRepository;
-import com.myservice.domain.article.util.BlogArticleItemParser;
 import com.myservice.service.util.UrlList;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,12 +46,11 @@ public class ApiRequestService {
     List<ApiResponseSaveDto> apiResponseSaveDtoList
         = getApiResponseSaveDtoList(userId, keyword, urlList);
 
-    List<ApiResponse> saveList = new ArrayList<>();
+    List<ApiResponse> saveList = apiResponseSaveDtoList
+                                  .stream().map(ApiResponseSaveDto::toEntity)
+                                  .collect(Collectors.toList());
 
-    for(ApiResponseSaveDto dto : apiResponseSaveDtoList){
-      apiResponseRepository.save(dto.toEntity());
-    }
-
+    apiResponseRepository.saveAll(saveList);
     return apiResponseSaveDtoList.size();
   }
 
