98d498dd7b3cbdb675d403794b6dbdcb48284d2b
Terry <terryakishin0814@gmail.com>
2022-12-11 22:02:00 +0900

    refactor(BlogArticleItemParser) : Optional을 사용하여 null 발생시 DB 저장되는 대치값 생성

--- core/src/main/java/com/myservice/domain/article/util/BlogArticleItemParser.java
+++ core/src/main/java/com/myservice/domain/article/util/BlogArticleItemParser.java
@@ -1,5 +1,6 @@
 package com.myservice.domain.article.util;
 
+import java.util.Optional;
 import lombok.Getter;
 import org.json.simple.JSONObject;
 
@@ -18,11 +19,22 @@ public class BlogArticleItemParser {
   }
 
   private BlogArticleItemParser(JSONObject item) {
-    this.link = (String) item.get("link");
-    this.postdate = (String) item.get("postdate");
-    this.description = (String) item.get("description");
-    this.title = (String) item.get("title");
-    this.bloggerlink = (String) item.get("bloggerlink");
-    this.bloggername = (String) item.get("bloggername");
+    Optional<String> linkOption = Optional.ofNullable((String) item.get("link"));
+    this.link = linkOption.orElse("");
+
+    Optional<String> postdataOption = Optional.ofNullable((String) item.get("postdate"));
+    this.postdate = postdataOption.orElse("");
+
+    Optional<String> descriptionOption = Optional.ofNullable((String) item.get("description"));
+    this.description = descriptionOption.orElse("");
+
+    Optional<String> titleOption = Optional.ofNullable((String) item.get("title"));
+    this.title = titleOption.orElse("");
+
+    Optional<String> bloggerlinkOption = Optional.ofNullable((String) item.get("bloggerlink"));
+    this.bloggerlink = bloggerlinkOption.orElse("");
+
+    Optional<String> bloggernameOption = Optional.ofNullable((String) item.get("bloggername"));
+    this.bloggername = bloggernameOption.orElse("");
   }
 }
